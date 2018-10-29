73f52dd40e409c01aa66f584e7f65ce925b0a042
Trần Anh <tranducanh@admins-imac.net.fpt>
2018-10-29 14:27:34 +0700

    Initial Commit

---
+++ TestGCD/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TestGCD
+//
+//  Created by Trần Đức Anh on 10/29/18.
+//  Copyright © 2018 Trần Đức Anh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
