de20157b4ceceb588cb661d69520251d9d52067d
Trần Đức Anh <ducanhxom4@gmail.com>
2018-11-01 08:55:25 +0700

    add

--- TestGCD/ViewController.swift
+++ TestGCD/ViewController.swift
@@ -12,6 +12,10 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        racingData()
+        simpleAsyncAfter()
+//        serialQueue()
+//        simpleConcurrentQueue()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
@@ -20,6 +24,46 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+//    func serialQueue() {
+//        //Khai báo
+//        let serialQueue = DispatchQueue(label: "com.bigZero.serialQueue")
+//        for i in 1...5 {
+//            serialQueue.async {                                                 //use Sync or Async for SerialQueue
+//                print("task: \(i): - \(Thread.current)")
+//                sleep(1)
+//            }
+//        }
+//        print("complete! - \(Thread.current)")
+//    }
 
+//    func simpleConcurrentQueue() {
+//        let concurrentQueue = DispatchQueue(label: "com.bigZero.concurrentQueue", attributes: .concurrent)
+//        for i in 1...10 {
+//            concurrentQueue.async {                                             //use Sync or Async for ConcurrentQueue
+//                print("Task: \(i) - \(Thread.current)")
+//                sleep(1)
+//            }
+//        }
+//        print("Complete! - \(Thread.current)")
+//    }
+                                                                            //RacingData
+    func racingData() {
+        let queue = DispatchQueue(label: "com.bigZero.concurrentQueueRacing", attributes: .concurrent)
+        var count = 0
+        for _ in 1...25000 {
+            queue.async {
+                count += 1
+                print(count)
+            }
+        }
+    }
+                                                                            //Dispatch AsyncAfter
+    func simpleAsyncAfter() {
+        let queue = DispatchQueue(label: "com.bigZero.queue")
+        queue.asyncAfter(deadline: .now() + 2) {
+            print("Simple asyncAfter")
+        }
+    }
+    
 }
 
